e88197da801f39413316fa82be407a2ec4fa0208
nOvelean <118316628+nOvelean@users.noreply.github.com>
2025-09-09 10:03:19 +0300

    Переделал работу

--- lab4/LanguageSwap.go
+++ lab4/LanguageSwap.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 )
+
+type PageData struct {
+	PageTitle    string
+	Welcome      string
+	LangButtonEn string
+	LangButtonRu string
+}
+
+var langData map[string]map[string]string
 
 func check(err error) {
 	if err != nil {
@@ -13,22 +24,54 @@ func check(err error) {
 		panic(err)
 	}
 }
+
+func loadLangData() {
+	langData = make(map[string]map[string]string)
+
+	enData, err := os.ReadFile("en.json")
+	check(err)
+	var enMap map[string]string
+	json.Unmarshal(enData, &enMap)
+	langData["en"] = enMap
+
+	ruData, err := os.ReadFile("ru.json")
+	check(err)
+	var ruMap map[string]string
+	json.Unmarshal(ruData, &ruMap)
+	langData["ru"] = ruMap
+}
+
 func enterMain(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "", 301)
-	QuaeryParam := r.URL.Query()
-	name := QuaeryParam.Get("name")
-	language := QuaeryParam.Get("language")
+	queryParam := r.URL.Query()
+	name := queryParam.Get("name")
+	lang := queryParam.Get("language")
+	if lang == "" {
+		lang = "en"
+	}
+	data, exists := langData[lang]
+	if !exists {
+		lang = "en"
+		data = langData["en"]
+	}
+
 	if name == "" {
-		Temp_EnterPage_html, err := os.ReadFile("C:/3CursETU/Web-Programming/lab1/EnterPage.html")
+		tmpl, err := template.ParseFiles("EnterPage.html")
 		check(err)
-		EnterPage_html := string(Temp_EnterPage_html)
-		fmt.Fprint(w, EnterPage_html)
+		pageData := PageData{
+			PageTitle:    data["page_title"],
+			Welcome:      data["welcome"],
+			LangButtonEn: data["language_button_en"],
+			LangButtonRu: data["language_button_ru"],
+		}
+		tmpl.Execute(w, pageData)
 	} else {
-		fmt.Fprintf(w, "Hello, %s, Welcome to our comunity", name)
+		greeting := fmt.Sprintf(data["greeting"], name)
+		fmt.Fprint(w, greeting)
 	}
 }
+
 func main() {
-	//port, err := strconv.Atoi(fmt.Scan())
+	loadLangData()
 	fmt.Println("Работаем")
 	http.HandleFunc("/", enterMain)
 	http.ListenAndServe(":3000", nil)
